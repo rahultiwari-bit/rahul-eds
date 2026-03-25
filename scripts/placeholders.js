ceb87da61fa799a830eb0e8a6f8f1a870eecc680
rahultiwari-bit <rahul.tiwari@deptagency.com>
2026-03-25 15:29:18 +0530

    Added Carousel

---
+++ scripts/placeholders.js
@@ -0,0 +1,10 @@
+export default async function fetchPlaceholders() {
+  return {
+    carousel: 'Carousel',
+    carouselSlideControls: 'Carousel Slide Controls',
+    previousSlide: 'Previous Slide',
+    nextSlide: 'Next Slide',
+    showSlide: 'Show Slide',
+    of: 'of',
+  };
+}
